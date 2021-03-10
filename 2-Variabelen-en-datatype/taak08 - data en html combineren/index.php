4bd47f91852a0e268a806c5f4436f9c23f9ed00c
ImranKaouch <2122928@talnet.nl>
2021-03-10 13:37:50 +0100

    taak 08

---
+++ 2-Variabelen-en-datatype/taak08 - data en html combineren/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<ul>
+<?php
+$voornaam1 = "Imran";
+$voornaam2 = "Zakaria";
+$voornaam3 = "Ismail";
+echo "<li> $voornaam1</li> <li> $voornaam2</li> <li> $voornaam3</li>";
+?>
+
+</ul>
+    
+</body>
+</html>
