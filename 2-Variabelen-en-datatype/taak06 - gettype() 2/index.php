8a9e04cd77c81c2cb907db5e4efd8a60401dba9a
ImranKaouch <2122928@talnet.nl>
2021-03-10 12:07:59 +0100

    taak06

---
+++ 2-Variabelen-en-datatype/taak06 - gettype() 2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$s = "een string";
+$i = 16;
+$f = 1.6;
+$b = true;
+$x = null;
+echo gettype ($s);
+echo" ";
+echo gettype ($i);
+echo" ";
+echo gettype ($f);
+echo" ";
+echo gettype ($b);
+echo" ";
+echo gettype ($x);
+?>
+</body>
+</html>
