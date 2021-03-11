1597e6c8e1d6b8b7de483e24912c890053a40ef2
ImranKaouch <2122928@talnet.nl>
2021-03-11 12:55:33 +0100

    taak01

---
+++ 3-Arrays/taak01 - Array variabele/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<ul>
+<?php
+$myMovies = array("<li>Mission Impossible</li>", "<li>Avangers Endgame</li>", "<li>Bad boys</li>");
+echo $myMovies{0}, $myMovies {1}, $myMovies{2};
+?>
+</ul>
+</body>
+</html>
