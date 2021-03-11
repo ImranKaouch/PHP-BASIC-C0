798ed7ffd0808cd6fe0fbd0f5f8918c7d380c42c
ImranKaouch <2122928@talnet.nl>
2021-03-11 14:33:04 +0100

    taak04

---
+++ 3-Arrays/taak04 - Multidimensionaal/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$abcGetallen = [ ["a","b","c","d"], ["24","56","78","90"] ];
+
+echo $abcGetallen[0] [0];
+echo $abcGetallen[1] [1];
+echo "<br>";
+echo $abcGetallen[0] [2];
+echo $abcGetallen[1] [3];
+?>
+
+    
+</body>
+</html>
