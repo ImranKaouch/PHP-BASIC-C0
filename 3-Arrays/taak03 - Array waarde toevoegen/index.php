5ece192cf70f18019310d2bc61a4f11458004878
ImranKaouch <2122928@talnet.nl>
2021-03-11 14:05:54 +0100

    taak03

---
+++ 3-Arrays/taak03 - Array waarde toevoegen/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$myHeroes = array ("Superman");
+$myHeroes [1] = ("Aquaman");
+array_push($myHeroes, 'Wonder Women');
+print_r($myHeroes);
+
+?>
+
+</body>
+</html>
