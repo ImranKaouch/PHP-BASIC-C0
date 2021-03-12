f525158cc8e60e6f78f6acaadb58be868b8de60d
ImranKaouch <2122928@talnet.nl>
2021-03-12 13:33:37 +0100

    taak 01

--- 4-Arithmetics/taak01 - Arithmetic Operators/index.php
+++ 4-Arithmetics/taak01 - Arithmetic Operators/index.php
@@ -4,7 +4,13 @@
 <?php
 $getal1 = 5;
 $getal2 = 10;
+echo $getal1 + $getal2;
+echo "<br>";
+echo $getal2 - $getal1;
+echo "<br>";
 echo $getal1 * $getal2;
+echo "<br>";
+echo $getal2 / $getal1;
 ?>    
 </body>
 </html>
