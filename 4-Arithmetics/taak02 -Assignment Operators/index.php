825f19df918d58e2355bccb9ff7d0d7ec7ddbae1
ImranKaouch <2122928@talnet.nl>
2021-03-12 14:05:48 +0100

    taak02

---
+++ 4-Arithmetics/taak02 -Assignment Operators/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$getal1 = 10;
+$getal1 += 5;
+echo "<br>";
+echo ($getal1);
+echo "<br>";
+$getal1 -= 10;
+echo ($getal1);
+$getal1 *= 3;
+echo "<br>";
+echo ($getal1);
+$getal1 /= 2;
+echo "<br>";
+echo ($getal1);
+
+
+?>
+</body>
+</html>
