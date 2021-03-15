eb409921a047474eb61090b2106fd9cf0274fa0d
ImranKaouch <2122928@talnet.nl>
2021-03-15 11:18:09 +0100

    taak01

---
+++ 5-Conditional_Statements/taak01 - Comparison Operators/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+ <?php 
+ $tekst1 = 12;
+ $tekst2 = "Gewoon tekst";
+ var_dump ($tekst1 == $tekst2);
+ echo "<br>";
+ var_dump ($tekst1 === $tekst2);
+ echo "<br>";
+ var_dump ($tekst1 != $tekst2);
+ echo "<br>";
+ var_dump ($tekst1 > $tekst2);
+ echo "<br>";
+ var_dump ($tekst1 < $tekst2);
+ echo "<br>";
+ var_dump ($tekst1 <> $tekst2);
+ echo "<br>";
+ var_dump ($tekst1 >= $tekst2);
+ echo "<br>";
+ var_dump ($tekst1 <= $tekst2);
+ ?>   
+</body>
+</html>
