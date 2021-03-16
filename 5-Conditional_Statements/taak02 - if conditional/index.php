ae7faf82d3f53300444b6ada5469fe2d03fdccd0
ImranKaouch <2122928@talnet.nl>
2021-03-16 11:47:19 +0100

    taak02

---
+++ 5-Conditional_Statements/taak02 - if conditional/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$test1 = 10;
+$test2 = 6;
+if ($test1 = $test2);
+echo "Ze zijn ongelijk aan elkaar";
+echo "<br>";
+if ($test1 === $test2);
+echo "Ze zijn identiek aan elkaar";
+echo "<br>";
+if ($test1 > $test2);
+echo "Test1 is groter dan test2";
+echo "<br>";
+if ($test1 >= $test2);
+echo "Test1 is groter of gelijk aan test2";
+?>    
+</body>
+</html>
