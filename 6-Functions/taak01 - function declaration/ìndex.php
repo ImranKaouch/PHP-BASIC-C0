1e303e3df0626c66947e542d54997cd67fcf2186
ImranKaouch <2122928@talnet.nl>
2021-03-17 14:05:14 +0100

    taak01

---
+++ 6-Functions/taak01 - function declaration/ìndex.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+function zetTweeWoordenAanElkaar() {
+    $woordje = "Hallo";
+    $zinnetje = " Mooie blauwe planeet!";
+    $heleZin = $woordje . $zinnetje;
+    echo ($heleZin);
+}
+zetTweeWoordenAanElkaar ();
+?>
+    
+</body>
+</html>
