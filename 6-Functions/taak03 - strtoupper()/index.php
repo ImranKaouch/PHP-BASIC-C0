a23b5c8bd8cf54be15a4952e7c8b8710e54e9094
ImranKaouch <2122928@talnet.nl>
2021-03-18 12:57:26 +0100

    taak03

---
+++ 6-Functions/taak03 - strtoupper()/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+function telAlleWoorden () {
+    $zin = "Mijn blauwe auto is een mooie snelle wagen";
+    $lengteZin = strlen ($zin);
+    echo ($lengteZin);
+}
+telAlleWoorden ();
+?>
+    
+</body>
+</html>
