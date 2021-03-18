ab84e334710cfec05763a4d6cd4b18adf6e9d981
ImranKaouch <2122928@talnet.nl>
2021-03-18 23:48:40 +0100

    taak02

---
+++ 7-Working with files/taak02 - require/includess/menu.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Menu</title>
+</head>
+<body>
+<ul>
+<li> menu-list1 </li>
+<li> menu-list2 </li>
+<li> menu-list3 </li>
+</ul>
+
+</body>
+</html>
