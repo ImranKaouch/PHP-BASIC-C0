cd2259ee8cf3f917342bc1abd8742f3b8459a964
ImranKaouch <2122928@talnet.nl>
2021-03-23 11:46:34 +0100

    taak03

--- 7-Working with files/taak03 - html & php/index.php
+++ 7-Working with files/taak03 - html & php/index.php
@@ -1,10 +1,23 @@
+<?php
+$leeftijd = 16;
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
     <title>Document</title>
 </head>
 <body>
+<div id="isMeerderJarig">
+<?php
+if ($leeftijd  < 18 ) {
+    echo "<h4> Je bent te jong om deze pagina te bekijken</h4>";
+}
+?>
+
+
+ </div>
     
 </body>
 </html>
